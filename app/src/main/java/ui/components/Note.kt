940c835e34273cf2a614d45ca8099fa5e682b70a
nastyapere <pavevaa1@mail.ru>
2023-03-19 23:23:47 +0300

    AppDrawer1

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -1,9 +1,7 @@
 package ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,17 +12,21 @@ import com.topic2.android.notes.theme.rwGreen
 
 @Composable
 fun Note(){
-    Box(
-        modifier = Modifier
-            .size(40.dp)
-            .background(rwGreen)
-    )
-    Text(text = "Заголовок", maxLines = 1)
-    Text(text = "Содержимое", maxLines = 1)
-    Checkbox(
-        checked = false, onCheckedChange = { },
-        modifier = Modifier.padding(start = 8.dp)
-    )
+    Row(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(rwGreen)
+        )
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Заголовок", maxLines = 1)
+            Text(text = "Содержимое", maxLines = 1)
+        }
+        Checkbox(
+            checked = false, onCheckedChange = { },
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
 }
 @Preview
 @Composable
