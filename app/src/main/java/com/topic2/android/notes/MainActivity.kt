539544f6d8114d3aa73173b9a34c5c01874e0554
nastyapere <pavevaa1@mail.ru>
2023-03-20 00:20:04 +0300

    SetContent

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.topic2.android.notes
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.rememberCoroutineScope
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.theme.NotesTheme
 import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
+import kotlinx.coroutines.launch
+import ui.components.AppDrawer
+import ui.components.Note
 
 /**
  * Main activity приложения.
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), Parcelable {
 
   private val viewModel: MainViewModel by viewModels(factoryProducer = {
     MainViewModelFactory(
@@ -19,11 +31,53 @@ class MainActivity : AppCompatActivity() {
     )
   })
 
+  constructor(parcel: Parcel) : this() {
+  }
+
+  @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
+      NotesTheme {
+        val coroutineScope = rememberCoroutineScope()
+        val scaffoldState: ScaffoldState = rememberScaffoldState()
 
+        Scaffold(
+          scaffoldState = scaffoldState,
+          drawerContent = {
+            AppDrawer(
+              currentScreen = Screen.Notes,
+              closeDrawerAction = {
+                coroutineScope.launch {
+                  scaffoldState.drawerState.close()
+                }
+              }
+            )
+          },
+          content = {
+            Note()
+          }
+        )
+      }
+    }
+  }
+
+  override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+  }
+
+  override fun describeContents(): Int {
+    return 0
+  }
+
+  companion object CREATOR : Parcelable.Creator<MainActivity> {
+    override fun createFromParcel(parcel: Parcel): MainActivity {
+      return MainActivity(parcel)
+    }
+
+    override fun newArray(size: Int): Array<MainActivity?> {
+      return arrayOfNulls(size)
     }
   }
 }
