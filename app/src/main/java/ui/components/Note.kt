9dc14c6176b5d5b111d9506a917d2c00f77d1628
nastyapere <pavevaa1@mail.ru>
2023-03-20 19:19:43 +0300

    Notecolor2

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -11,8 +11,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.topic2.android.notes.theme.rwGreen
 
 @Composable
@@ -27,16 +30,33 @@ fun Note(){
     ) {
 
         NoteColor(
+            modifier = Modifier.align(Alignment.CenterVertically)
+                .padding(start = 16.dp, end = 16.dp),
             color = rwGreen,
             size =40.dp,
-            padding = 4.dp,
             border = 1.dp
         )
         Column(modifier = Modifier.weight(1f)
             .align(Alignment.CenterVertically)
         ) {
-            Text(text = "Заголовок", maxLines = 1)
-            Text(text = "Содержимое", maxLines = 1)
+            Text(text = "Заголовок",
+            color = Color.Black,
+                maxLines = 1,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 16.sp,
+                    letterSpacing = 0.15.sp
+                )
+                )
+            Text(text = "Содержимое",
+                color = Color.Black.copy(alpha = 0.75f),
+                maxLines = 1,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp,
+                    letterSpacing = 0.25.sp
+                )
+            )
         }
         Checkbox(
             checked = false, onCheckedChange = { },
