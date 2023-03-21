7dd7f0386d553046feac330f6913cb656d84939e
nastyapere <pavevaa1@mail.ru>
2023-03-21 21:15:46 +0300

    MainACtivity

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.topic2.android.notes
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import com.topic2.android.notes.routing.NotesRouter
 import com.topic2.android.notes.routing.Screen
 import com.topic2.android.notes.screens.NotesScreen
 import com.topic2.android.notes.theme.NotesTheme
-import com.topic2.android.notes.ui.screens.TrashScreen
 import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
 import ui.components.screens.SaveNoteScreen
@@ -29,25 +28,26 @@ class MainActivity : AppCompatActivity() {
   })
 
 
-  @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+  @ExperimentalMaterialApi
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
       NotesTheme {
-      NotesScreen(viewModel = viewModel)
+        MainActivityScreen(viewModel = viewModel)
+      }
     }
-  }}}
+  }
+}
 
 @Composable
-@ExperementalMaterialApi
-private fun MainActivityScreen(viewModel: MainViewModel){
-  Surface {
+@ExperimentalMaterialApi
+private fun MainActivityScreen(viewModel: MainViewModel) {
+  Surface{
     when (NotesRouter.currentScreen){
       is Screen.Notes -> NotesScreen(viewModel)
       is Screen.SaveNote -> SaveNoteScreen(viewModel)
-      is Screen.Trash -> TrashScreen(viewModel)
+      is Screen.Trash -> Screen.Trash
     }
-
   }
 }
